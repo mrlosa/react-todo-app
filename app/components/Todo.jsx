5986a78d6eac4b46678816335a9f3070e5cc6952
Roberto Riutort <rriutort@leapfrog.com>
2017-06-15 14:07:14 -0700

    Add completed and created tiemstamps

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,15 +1,28 @@
 var React = require('react');
+var moment = require('moment');
 
 var Todo = React.createClass({
   render: function(){
-    var {id, text, completed} = this.props;
+    var {id, text, completed, createdAt, compleatedAt} = this.props;
+    var renderDate = () => {
+      var message = 'Created ';
+      var timestamp = createdAt;
+
+      if (completed) {
+        message = 'Completed ';
+        timestamp = compleatedAt;
+      }
+
+      return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
+    };
     return (
       // Warning onClick: Failed form propType: You provided a `checked` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultChecked`. Otherwise, set either `onChange` or `readOnly`. Check the render method of `Todo`.
       <div onClick={()=> {
         this.props.onToggle(id);
       }}>
         <input type="checkbox" checked={completed}/>
-        {text}
+        <p>{text}</p>
+        <p>{renderDate()}</p>
       </div>
     )
   }
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -20,22 +20,54 @@ describe('TodoApp', () => {
 
     // we grab the text value of the todos and verify it
     expect(todoApp.state.todos[0].text).toBe(todoText);
+
+    // Expect createdAt to be a number
+    expect(todoApp.state.todos[0].createdAt).toBeA('number');
   });
 
   it('should toggle completed value when handleToggle called', () => {
     var todoData = {
       id: 11,
       text: 'Test features',
-      completed: false
+      completed: false,
+      createdAt: 0,
+      completedAt: undefined
     };
     var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
     todoApp.setState({todos: [todoData]});
 
-    // chaeck that todos first itme has completed value of false
+    // check that todos first itme has completed value of false
     expect(todoApp.state.todos[0].completed).toBe(false);
     //call handleToggle with 11
     todoApp.handleToggle(11);
     //Verify that value changed
     expect(todoApp.state.todos[0].completed).toBe(true);
+
+    // Expect completedAt to be a number
+    expect(todoApp.state.todos[0].completedAt).toBeA('number');
   });
+
+  // Test that when toggle from true to false, compleatedAt get removed
+  it('should toggle todo from completed to incomplete', () => {
+    var todoData = {
+      id: 11,
+      text: 'Test features',
+      completed: true,
+      createdAt: 0,
+      completedAt: 123
+    };
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+    todoApp.setState({todos: [todoData]});
+
+    // check that todos first itme has completed value of false
+    expect(todoApp.state.todos[0].completed).toBe(true);
+    //call handleToggle with 11
+    todoApp.handleToggle(11);
+    //Verify that value changed
+    expect(todoApp.state.todos[0].completed).toBe(false);
+
+    // Expect completedAt to be a number
+    expect(todoApp.state.todos[0].completedAt).toNotExist();
+  });
+
 });
