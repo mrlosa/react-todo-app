54a4ac0db476a3bb9738ece83a49fd2d1b7fc1d7
Roberto Riutort <rriutort@leapfrog.com>
2017-06-28 15:52:49 -0700

    Add todos reducers and tests

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,3 +1,6 @@
+var uuid = require('node-uuid');
+var moment = require('moment');
+
 export var searchTextReducer = (state ='', action) => {
   switch (action.type) {
     case 'SET_SEARCH_TEXT':
@@ -12,6 +15,37 @@ export var showCompletedReducer = (state = false, action) => {
     case 'TOGGLE_SHOW_COMPLETED':
       return !state;
       break;
+    default:
+      return state;
+  }
+};
+
+export var todosReducer = (state= [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id:  uuid(),
+          text: action.text,
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        }
+      ]
+      break;
+      case 'TOGGLE_TODO':
+        return state.map((todo)=> {
+          if(todo.i === action.i) {
+            var nextCompleted = !todo.completed;
+            return {
+              ...todo,
+              completed: nextCompleted,
+              completedAt:nextCompleted ? moment().unix(): undefined
+            };
+          }
+        });
+        break;
     default:
       return state;
   }
